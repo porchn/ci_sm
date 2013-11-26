7f4a0f5136aa62c44b56e988e0808d82000367f7
pichai chin <pichai.chin@gmail.com>
2013-11-26 09:32:25 +0700

    upload data

---
+++ _CI2/app/models/product_model.php
@@ -0,0 +1,19 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Product_model extends MY_Model {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function setProduct()
+	{
+		
+	}
+
+	public function getProduct()
+	{
+
+	}
+}
